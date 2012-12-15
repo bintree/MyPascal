de0b3b4ec318fd38b6b1ba9c5b7dc6c12beaf73e
Denis Zharkov <bintree@eblans.org>
2012-12-15 13:45:07 +0600

    test for checking constanst and global assignment work

---
+++ tests/test_constants_assignment.pas
@@ -0,0 +1,15 @@
+var 
+a,b,c: integer;
+d: double;
+e: string;
+begin
+a:=next_integer();
+c:=a;
+b:=1;
+println_integer(c);
+println_integer(b);
+d:=4.5;
+println_double(d);
+e:=next_string();
+println_string(e);
+end.
